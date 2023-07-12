04fc7b58a011456ef0e54feae5bfcb0cce85f123
Kelsey379 <kelseyroughpng@gmail.com>
2023-07-11 23:07:14 -0400

    added voltage toggles

--- src/updateSchema.py
+++ src/updateSchema.py
@@ -27,16 +27,16 @@ if led_group is not None:
 
     # Generate LED elements dynamically based on the number of LED elements in the data
     for led_element in led_elements:
-        led_control = {
-            "type": "Control",
-            "scope": f"#/properties/registerMap/properties/LEDs/properties/{led_element['name']}",
-            "options": {
-                "type": "boolean",
-                "toggle": True
+        if led_element['bitWidth'] == 1:
+            led_control = {
+                "type": "Control",
+                "scope": f"#/properties/registerMap/properties/LEDs/properties/{led_element['name']}",
+                "options": {
+                    "type": "boolean",
+                    "toggle": True
+                }
             }
-        }
-        led_group['elements'].append(led_control)
-
+            led_group['elements'].append(led_control)
 
 # Find the VEn group in the uischema
 ven_group = None
@@ -51,19 +51,16 @@ if ven_group is not None:
 
     # Generate VEn elements dynamically based on the number of VEn elements in the data
     for ven_element in ven_elements:
-        ven_control = {
-            "type": "Control",
-            "scope": f"#/properties/registerMap/properties/VEn/properties/{ven_element['name']}",
-            "options": {
-                "inputAttributes": {
-                    "type": "number",
-                    "step": 1,
-                    "pattern": "\\d+"
+        if ven_element['bitWidth'] == 1:
+            ven_control = {
+                "type": "Control",
+                "scope": f"#/properties/registerMap/properties/VEn/properties/{ven_element['name']}",
+                "options": {
+                    "type": "boolean",
+                    "toggle": True
                 }
             }
-        }
-        ven_group['elements'].append(ven_control)
-
+            ven_group['elements'].append(ven_control)
 
 # Find the Clock group in the uischema
 clock_group = None
@@ -78,28 +75,31 @@ if clock_group is not None:
 
     # Generate Clock_High elements dynamically based on the number of Clock_High elements in the data
     for clock_high_element in clock_high_elements:
-        clock_high_control = {
-            "type": "Control",
-            "scope": f"#/properties/registerMap/properties/Clock/properties/Clock_High/properties/{clock_high_element['name']}",
-            "options": {
-                "type": "string",
-                "readonly": True
+        if clock_high_element['bitWidth'] == 1:
+            clock_high_control = {
+                "type": "Control",
+                "scope": f"#/properties/registerMap/properties/Clock/properties/Clock_High/properties/{clock_high_element['name']}",
+                "options": {
+                    "type": "boolean",
+                    "toggle": True,
+                    "readonly": True
+                }
             }
-        }
-        clock_group['elements'].append(clock_high_control)
+            clock_group['elements'].append(clock_high_control)
 
     # Generate Clock_Low elements dynamically based on the number of Clock_Low elements in the data
     for clock_low_element in clock_low_elements:
-        clock_low_control = {
-            "type": "Control",
-            "scope": f"#/properties/registerMap/properties/Clock/properties/Clock_Low/properties/{clock_low_element['name']}",
-            "options": {
-                "type": "string",
-                "readonly": True
+        if clock_low_element['bitWidth'] == 1:
+            clock_low_control = {
+                "type": "Control",
+                "scope": f"#/properties/registerMap/properties/Clock/properties/Clock_Low/properties/{clock_low_element['name']}",
+                "options": {
+                    "type": "boolean",
+                    "toggle": True,
+                    "readonly": True
+                }
             }
-        }
-        clock_group['elements'].append(clock_low_control)
-
+            clock_group['elements'].append(clock_low_control)
 
 # Save the updated uischema.json file
 with open('src/uischema.json', 'w') as uischema_file:
@@ -151,22 +151,24 @@ for led_element in led_elements:
 # Generate schema properties for VEn elements
 for ven_element in ven_elements:
     schema["properties"]["registerMap"]["properties"]["VEn"]["properties"][ven_element["name"]] = {
-        "type": "integer",
+        "type": "boolean" if ven_element['bitWidth'] == 1 else "integer",
         "title": ven_element["name"]
     }
 
 # Generate schema properties for Clock_High elements
 for clock_high_element in clock_high_elements:
     schema["properties"]["registerMap"]["properties"]["Clock"]["properties"]["Clock_High"]["properties"][clock_high_element["name"]] = {
-        "type": "string",
-        "title": clock_high_element["name"]
+        "type": "boolean" if clock_high_element['bitWidth'] == 1 else "string",
+        "title": clock_high_element["name"],
+        "readOnly": True
     }
 
 # Generate schema properties for Clock_Low elements
 for clock_low_element in clock_low_elements:
     schema["properties"]["registerMap"]["properties"]["Clock"]["properties"]["Clock_Low"]["properties"][clock_low_element["name"]] = {
-        "type": "string",
-        "title": clock_low_element["name"]
+        "type": "boolean" if clock_low_element['bitWidth'] == 1 else "string",
+        "title": clock_low_element["name"],
+        "readOnly": True
     }
 
 # Save the updated schema.json file
